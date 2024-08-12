b625f241fe239338e334000dd636577cc6171087
Yolonek <thejgrabowski@gmail.com>
2024-08-12 17:05:29 +0200

    added psiformer

--- Models.py
+++ Models.py
@@ -42,3 +42,67 @@ class FeedForwardNeuralNetwork(nn.Module):
                                     bias_init=jax.nn.initializers.normal(stddev=self.stddev))(x))
         return jnp.sum(x, axis=-1)
 
+
+class DotProductAttention(nn.Module):
+
+    @nn.compact
+    def __call__(self, query, key, value):
+        d_k = query.shape[-1]
+        scores = jnp.matmul(query, key.swapaxes(-2, -1)) / jnp.sqrt(d_k)
+        attention_weights = nn.softmax(scores, axis=-1)
+        return jnp.matmul(attention_weights, value), attention_weights
+
+
+class MultiHeadAttention(nn.Module):
+    num_heads: int
+    head_dim: int
+
+    @nn.compact
+    def __call__(self, x):
+        batch_size, seq_len, emb_dim = x.shape
+        qkv = nn.Dense(self.num_heads * self.head_dim * 3)(x)
+        qkv = qkv.reshape(batch_size, seq_len, self.num_heads, 3 * self.head_dim)
+        query, key, value = jnp.split(qkv, 3, axis=-1)
+
+        attention_outputs, _ = DotProductAttention()(query, key, value)
+        attention_outputs = attention_outputs.reshape(batch_size, seq_len, -1)
+        return nn.Dense(self.head_dim * self.num_heads)(attention_outputs)
+
+
+class TransformerBlock(nn.Module):
+    num_heads: int
+    head_dim: int
+    mlp_dim: int
+
+    @nn.compact
+    def __call__(self, x, deterministic=True):
+        y = nn.LayerNorm()(x)
+        y = MultiHeadAttention(num_heads=self.num_heads, head_dim=self.head_dim)(y)
+        x = x + y
+
+        y = nn.LayerNorm()(x)
+        y = nn.Dense(self.mlp_dim)(y)
+        y = nn.relu(y)
+        y = nn.Dense(self.head_dim * self.num_heads)(y)
+        return x + y
+
+
+class Psiformer(nn.Module):
+    num_heads: int
+    head_dim: int
+    mlp_dim: int
+    num_layers: int
+    max_length: int
+
+    @nn.compact
+    def __call__(self, x):
+        pos_embedding = self.param('pos_embedding',
+                                   nn.initializers.normal(stddev=0.02),
+                                   (1, x.shape[1], self.num_heads * self.head_dim))
+        x = nn.Embed(self.max_length, self.num_heads * self.head_dim)(x) + pos_embedding
+
+        for _ in range(self.num_layers):
+            x = TransformerBlock(self.num_heads, self.head_dim, self.mlp_dim)(x)
+
+        x = nn.LayerNorm()(x)
+        return x.mean(axis=1)
