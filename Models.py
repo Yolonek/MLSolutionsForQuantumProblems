696e424d9329decfab6a7d458cd70e566794d066
Yolonek <thejgrabowski@gmail.com>
2024-08-14 10:40:38 +0200

    Model comparison

--- Models.py
+++ Models.py
@@ -3,12 +3,13 @@ import jax.numpy as jnp
 import flax.linen as nn
 import netket.nn as nknn
 import netket as nk
+from typing import Union, Any, Tuple, Callable
 
 
 class RestrictedBoltzmannMachine(nn.Module):
-    param_dtype = jnp.float64
-    alpha = 1
-    stddev = 0.01
+    param_dtype: Any = jnp.float64
+    alpha: Union[float, int] = 1
+    stddev: float = 0.01
 
     @nn.compact
     def __call__(self, x):
@@ -19,32 +20,84 @@ class RestrictedBoltzmannMachine(nn.Module):
                          kernel_init=jax.nn.initializers.normal(stddev=self.stddev),
                          bias_init=jax.nn.initializers.normal(stddev=self.stddev))
         bias = self.param('visible_bias',
-                          self.jax.nn.initializers.normal(stddev=self.stddev),
+                          jax.nn.initializers.normal(stddev=self.stddev),
                           (x.shape[-1],),
                           self.param_dtype)
         return jnp.sum(nknn.log_cosh(layer(x)), axis=-1) + jnp.dot(x, bias)
 
 
 class FeedForwardNeuralNetwork(nn.Module):
-    param_dtype = jnp.float64
-    layer_alpha = [1]
-    stddev = 0.01
+    param_dtype: Any = jnp.float64
+    layer_alpha: Tuple[int] = (1,)
+    stddev: float = 0.01
+    activation: Callable = nknn.log_cosh
 
     @nn.compact
     def __call__(self, x):
-        activation = nknn.log_cosh
         for index, alpha in enumerate(self.layer_alpha):
-            x = activation(nn.Dense(f'Dense{index}',
-                                    features=int(alpha * x.shape[-1]),
-                                    param_dtype=self.param_dtype,
-                                    use_bias=True,
-                                    kernel_init=jax.nn.initializers.normal(stddev=self.stddev),
-                                    bias_init=jax.nn.initializers.normal(stddev=self.stddev))(x))
+            x = self.activation(nn.Dense(name=f'Dense{index + 1}',
+                                         features=int(alpha * x.shape[-1]),
+                                         param_dtype=self.param_dtype,
+                                         use_bias=True,
+                                         kernel_init=jax.nn.initializers.normal(stddev=self.stddev),
+                                         bias_init=jax.nn.initializers.normal(stddev=self.stddev))(x))
+        return jnp.sum(x, axis=-1)
+
+
+class TransformerBlock(nn.Module):
+    num_heads: int = 1
+    head_dim: int = 1
+    alpha: Union[float, int] = 1
+    param_dtype: Any = jnp.float64
+    activation: Callable = nknn.log_cosh
+    stddev: float = 0.01
+
+    @nn.compact
+    def __call__(self, x):
+        residual = x
+        dense_params = dict(param_dtype=self.param_dtype,
+                            kernel_init=jax.nn.initializers.normal(stddev=self.stddev),
+                            bias_init=jax.nn.initializers.normal(stddev=self.stddev))
+        x = nn.LayerNorm()(x)
+        num_features = int(self.alpha * x.shape[-1])
+        qkv_features = self.num_heads * self.head_dim * 3 * x.shape[-1]
+        qkv = nn.Dense(features=qkv_features,
+                       use_bias=False,
+                       **dense_params)(x)
+        q, k, v = jnp.split(qkv, 3, axis=-1)
+        x = nn.MultiHeadDotProductAttention(num_heads=self.num_heads,
+                                            # qkv_features=qkv_features,
+                                            **dense_params)(q, k, v)
+        x = nn.Dense(features=num_features,
+                     use_bias=True,
+                     **dense_params)(x)
+        x = self.activation(x)
+        return nn.Dense(features=residual.shape[-1], use_bias=False, **dense_params)(x) + residual
+
+
+class Psiformer(nn.Module):
+    transformer_blocks: int = 1
+    num_heads: int = 2
+    alpha: Union[float, int] = 1
+    param_dtype: Any = jnp.float64
+    activation: Callable = nknn.log_cosh
+    stddev: float = 0.01
+
+    @nn.compact
+    def __call__(self, x):
+        print(x.shape)
+        for _ in range(self.transformer_blocks):
+            x = TransformerBlock(
+                num_heads=self.num_heads,
+                alpha=self.alpha,
+                param_dtype=self.param_dtype,
+                activation=self.activation,
+                stddev=self.stddev
+            )(x)
         return jnp.sum(x, axis=-1)
 
 
 class DotProductAttention(nn.Module):
-
     @nn.compact
     def __call__(self, query, key, value):
         d_k = query.shape[-1]
@@ -52,57 +105,54 @@ class DotProductAttention(nn.Module):
         attention_weights = nn.softmax(scores, axis=-1)
         return jnp.matmul(attention_weights, value), attention_weights
 
-
 class MultiHeadAttention(nn.Module):
     num_heads: int
     head_dim: int
 
     @nn.compact
     def __call__(self, x):
-        batch_size, seq_len, emb_dim = x.shape
-        qkv = nn.Dense(self.num_heads * self.head_dim * 3)(x)
-        qkv = qkv.reshape(batch_size, seq_len, self.num_heads, 3 * self.head_dim)
+        qkv = nn.Dense(self.head_dim * self.num_heads * 3)(x)
         query, key, value = jnp.split(qkv, 3, axis=-1)
-
         attention_outputs, _ = DotProductAttention()(query, key, value)
-        attention_outputs = attention_outputs.reshape(batch_size, seq_len, -1)
-        return nn.Dense(self.head_dim * self.num_heads)(attention_outputs)
+        # print(attention_outputs.shape)
+        # attention_outputs = attention_outputs.reshape(1,-1)
+        # print(attention_outputs.shape)
+        return nn.Dense(features=self.head_dim * self.num_heads)(attention_outputs)
 
-
-class TransformerBlock(nn.Module):
+class TransformerLayer(nn.Module):
     num_heads: int
     head_dim: int
     mlp_dim: int
 
     @nn.compact
-    def __call__(self, x, deterministic=True):
+    def __call__(self, x):
         y = nn.LayerNorm()(x)
         y = MultiHeadAttention(num_heads=self.num_heads, head_dim=self.head_dim)(y)
         x = x + y
 
         y = nn.LayerNorm()(x)
         y = nn.Dense(self.mlp_dim)(y)
-        y = nn.relu(y)
-        y = nn.Dense(self.head_dim * self.num_heads)(y)
+        y = nknn.log_cosh(y)
+        # y = nn.Dense(self.head_dim + self.num_heads)(y)
         return x + y
 
 
-class Psiformer(nn.Module):
-    num_heads: int
-    head_dim: int
-    mlp_dim: int
-    num_layers: int
-    max_length: int
+class PsiformerExtended(nn.Module):
+    num_heads: int = 4
+    head_dim: int = 16
+    mlp_dim: int = 64
+    num_layers: int = 1
 
     @nn.compact
     def __call__(self, x):
+        embedding_dim = self.num_heads * self.head_dim
         pos_embedding = self.param('pos_embedding',
                                    nn.initializers.normal(stddev=0.02),
-                                   (1, x.shape[1], self.num_heads * self.head_dim))
-        x = nn.Embed(self.max_length, self.num_heads * self.head_dim)(x) + pos_embedding
-
+                                   (1, embedding_dim))
+        x = nn.Dense(embedding_dim)(x)
+        x = x + pos_embedding
         for _ in range(self.num_layers):
-            x = TransformerBlock(self.num_heads, self.head_dim, self.mlp_dim)(x)
+            x = TransformerLayer(self.num_heads, self.head_dim, self.mlp_dim)(x)
 
         x = nn.LayerNorm()(x)
         return x.mean(axis=1)
